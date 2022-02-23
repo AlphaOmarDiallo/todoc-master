3dfa8d8e6d023c016910183bc32e039d5647eb70
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-02-23 12:42:35 +0100

    Project activity recyclerview displays projects and random color are displayed for each project.

--- app/src/main/java/com/cleanup/todoc/ui/project/AddProjectActivity.java
+++ app/src/main/java/com/cleanup/todoc/ui/project/AddProjectActivity.java
@@ -1,16 +1,57 @@
 package com.cleanup.todoc.ui.project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cleanup.todoc.R;
+import com.cleanup.todoc.model.Project;
+import com.cleanup.todoc.viewmodel.ProjectActivityViewModel;
 
+import java.util.List;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class AddProjectActivity extends AppCompatActivity {
+    ProjectActivityViewModel viewModel;
+    EditText projectName;
+    Button addNewProject;
+    Button cancel;
+    List<Project> allProjects;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        viewModel = new ViewModelProvider(this).get(ProjectActivityViewModel.class);
         setContentView(R.layout.activity_add_project);
+
+        projectName = findViewById(R.id.tv_project_name_aap);
+        addNewProject = findViewById(R.id.button_add_project_aap);
+        cancel = findViewById(R.id.button_annuler);
+
+        viewModel.getAllProjects().observe(this, this::getAllProjects);
+
+        addNewProject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (projectName.getText().toString().trim().length() == 0) {
+                    Toast.makeText(AddProjectActivity.this, R.string.empty_project_name, Toast.LENGTH_SHORT).show();
+                } else {
+                   viewModel.insertProject(viewModel.createProject(allProjects, projectName.getText().toString()));
+                }
+            }
+        });
+
+    }
+
+    private List<Project> getAllProjects(List<Project> projects){
+        allProjects = projects;
+        return allProjects;
     }
 }
--- app/src/main/java/com/cleanup/todoc/viewmodel/ProjectActivityViewModel.java
+++ app/src/main/java/com/cleanup/todoc/viewmodel/ProjectActivityViewModel.java
@@ -1,5 +1,12 @@
 package com.cleanup.todoc.viewmodel;
 
+import static android.content.ContentValues.TAG;
+
+import android.graphics.Color;
+import android.util.Log;
+
+import androidx.annotation.ColorInt;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -9,6 +16,7 @@ import com.cleanup.todoc.repository.ProjectRepositoryImpl;
 import com.cleanup.todoc.repository.TaskRepositoryImpl;
 
 import java.util.List;
+import java.util.Random;
 
 import javax.inject.Inject;
 
@@ -40,9 +48,40 @@ public class ProjectActivityViewModel extends ViewModel {
 
     public void insertProject(Project project) {
         projectRepositoryImpl.insertProject(project);
+        Log.e(TAG, "insertProject: new project added", null);
     }
 
     public void deleteProject(Project project) {
         projectRepositoryImpl.deleteProject(project);
+    }
+
+    public List<Project> currentList(List<Project> projects){
+        return projects;
+    }
+
+    public @ColorInt int randomColor(){
+        Random random = new Random();
+        int color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
+        return color;
+    }
+
+    public Project createProject(List<Project> projects, String name) {
+        long projectId;
+        int projectColor = randomColor();
+        // getting id
+        long greatestId = 0;
+        List<Project> listProject = projectRepositoryImpl.getAllProjects().getValue();
+        if (projects.isEmpty()) {
+            projectId = 1;
+        } else {
+            for(int i = 0; i < listProject.size(); i++) {
+                long id = listProject.get(i).getId();
+                if (id > greatestId){
+                    greatestId = id;
+                }
+            }
+            projectId = greatestId + 1;
+        }
+        return new Project(projectId, name, projectColor);
     }
 }
